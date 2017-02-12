58c52763db3eacf3ae8640372628805c0d13b63f
superlanglang <langlangsuzi@163.com>
2017-02-12 19:05:12 +0800

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//约束该类型必须是class，而且必须有一个不带参的构造函数。
+public class Singleton<T> where T : class,new()
+{
+    private static T mInstance;
+    public static T Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = Activator.CreateInstance<T>();
+            }
+            return mInstance;
+        }
+    }
+
+    protected Singleton()
+    {
+
+    }
+}
